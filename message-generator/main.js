4a83e71290f2df6add9a72dea243c28411274582
marleyrae <marleyrae@protonmail.com>
2023-06-23 11:25:50 -0700

    Write message parts

--- message-generator/main.js
+++ message-generator/main.js
@@ -1,2 +1,23 @@
 /* Fortune Teller Bot */
 
+const part1 = [
+  'I think no, ',
+  'I can\'t say for certain, ',
+  'You are in complete control, ',
+  'It isn\'t long before you\'ll know for sure, ',
+  'I\'d say yes, ',
+  'It doesn\'t seem possible, ',
+  'The result might not be what you are hoping for, ',
+]
+
+const part2 = [
+  'but you are the only one who can be sure.',
+  'but your gut knows.',
+  'sometimes it doesn’t seem that way though.',
+  'but the outcome looks positive.',
+  'this is my best guess.',
+  'but many things that don\'t seem possible actually are.',
+  'but it will turn out perfectly in the end!',
+]
+
+const part3 = 'Your lucky number is '
