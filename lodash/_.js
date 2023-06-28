534819ee572398b01b3e56bb5c94f3b5c260d4c1
marleyrae <marleyrae@protonmail.com>
2023-06-27 18:13:07 -0700

    Write _.clamp()

--- lodash/_.js
+++ lodash/_.js
@@ -1,2 +1,8 @@
+let _ = {}
+
+/* --------------------------------------------------------------- clamp ---- */
+
+_.clamp = (num, lower, higher) => Math.min(Math.max(num, lower), higher)
+
 // Do not write or modify code below this line.
 module.exports = _
