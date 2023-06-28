10d7c5a6866287f87fcb3eca3406e8b6bc4b0e99
marleyrae <marleyrae@protonmail.com>
2023-06-27 18:17:29 -0700

    Write .inRange()

--- lodash/_.js
+++ lodash/_.js
@@ -4,5 +4,22 @@ let _ = {}
 
 _.clamp = (num, lower, higher) => Math.min(Math.max(num, lower), higher)
 
+/* ------------------------------------------------------------- inRange ---- */
+
+_.inRange = function (num, start, end) {
+  if (!end) {
+    end = start
+    start = 0
+  }
+
+  if (start > end) {
+    let s = start
+    start = end
+    end = s
+  }
+
+  return start <= num && num < end
+}
+
 // Do not write or modify code below this line.
 module.exports = _
